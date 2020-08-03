bed48cd00ed47f8a431e47675462615b329ea3d0
MCLEANS <jackagwaya@gmail.com>
2020-08-03 19:16:19 +0300

    created i2c library files

---
+++ IMPLEMENTATION/src/I2C.cpp
@@ -0,0 +1,21 @@
+/*
+ * I2C.cpp
+ *
+ *  Created on: Aug 3, 2020
+ *      Author: MCLEANS
+ */
+
+#include "I2C.h"
+
+namespace custom_libraries {
+
+I2C::I2C() {
+	// TODO Auto-generated constructor stub
+
+}
+
+I2C::~I2C() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace custom_libraries */
---
+++ IMPLEMENTATION/src/I2C.h
@@ -0,0 +1,24 @@
+/*
+ * I2C.h
+ *
+ *  Created on: Aug 3, 2020
+ *      Author: MCLEANS
+ */
+
+#ifndef I2C_H_
+#define I2C_H_
+
+#include "stm32f10x.h"
+
+namespace custom_libraries {
+
+class I2C {
+public:
+	I2C();
+
+	~I2C();
+};
+
+} /* namespace custom_libraries */
+
+#endif /* I2C_H_ */
