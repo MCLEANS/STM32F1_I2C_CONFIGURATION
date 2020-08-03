bf146cb0c210c14405782319db289a88e8bf9120
MCLEANS <jackagwaya@gmail.com>
2020-08-03 19:13:37 +0300

    Added i2c functions

--- IMPLEMENTATION/src/main.cpp
+++ IMPLEMENTATION/src/main.cpp
@@ -42,7 +42,7 @@ uint8_t decimal_to_BCD(uint8_t val){
 	return((val /10 * 16) + (val %10));
 }
 
-void read(uint8_t address,uint8_t len){
+void read_bytes(uint8_t address,uint8_t *buffer,uint8_t len){
 	uint32_t temp = 0;
 	RCC->AHBENR |= RCC_AHBENR_DMA1EN;
 	//ENABLE I2C DMA
@@ -71,11 +71,9 @@ void read(uint8_t address,uint8_t len){
 	//SEND STOP BIT
 	I2C1->CR1 |= I2C_CR1_STOP;
 
-
-
 }
 
-void write_single(uint8_t address,uint8_t mem,uint8_t data){
+void write_byte(uint8_t address,uint8_t mem,uint8_t data){
 	uint32_t temp;
 	//GENERATE START CONDITION
 	I2C1->CR1 |= I2C_CR1_START;
@@ -100,7 +98,35 @@ void write_single(uint8_t address,uint8_t mem,uint8_t data){
 
 }
 
-void set_pointer(uint8_t address,uint8_t mem){
+void write_bytes(uint8_t address,uint8_t mem,uint8_t *data,uint16_t len){
+	uint32_t temp;
+	//GENERATE START CONDITION
+	I2C1->CR1 |= I2C_CR1_START;
+	//WAIT UNTIL START BIT IS SENT
+	while(!(I2C1->SR1 & I2C_SR1_SB)){};
+	//SEND ADDRESS
+	I2C1->DR = address;
+	//WAIT UNTIL ADDRESS IS SENT
+	while(!(I2C1->SR1 & I2C_SR1_ADDR)){}
+	//READ SR1 AND SR2 TO CLEAR THE BIT
+	temp = I2C1->SR2;
+	//ADDRESS TO WRITE TO
+	I2C1->DR = mem;
+	//WAIT FOR TRANSFER TO COMPLETE
+	while(!(I2C1->SR1 & I2C_SR1_TXE)){}
+	for(int i = 0; i < len; i++, *data++){
+		//DATA TO WRITE IN ADDRESS
+		I2C1->DR = *data;
+		//WAIT FOR TRANSFER TO COMPLETE
+		while(!(I2C1->SR1 & I2C_SR1_TXE)){}
+	}
+
+	//SET STOP BIT
+	I2C1->CR1 |= I2C_CR1_STOP;
+
+}
+
+void write_memp(uint8_t address,uint8_t mem){
 	uint32_t temp;
 	//GENERATE START CONDITION
 	I2C1->CR1 |= I2C_CR1_START;
@@ -121,21 +147,8 @@ void set_pointer(uint8_t address,uint8_t mem){
 
 }
 
-
-
-extern "C" void TIM1_UP_IRQHandler(void){
-	if(TIM1->SR & TIM_SR_UIF){
-		TIM1->SR &= ~TIM_SR_UIF;
-		tick_ms++;
-	}
-}
-
-
-int main(void)
-{
-
+void init(){
 	//ENABLE I2C RCC
-
 	RCC->APB1ENR &= ~RCC_APB1ENR_I2C1EN;
 	I2C1->CR2 = 0x00;
 	I2C1->CR1 = 0x00;
@@ -166,30 +179,39 @@ int main(void)
 
 	//ENABLE THE PERIPHERAL, MUST BE DONE LAST
 	I2C1->CR1 |= I2C_CR1_PE;
+}
+
+
+
+extern "C" void TIM1_UP_IRQHandler(void){
+	if(TIM1->SR & TIM_SR_UIF){
+		TIM1->SR &= ~TIM_SR_UIF;
+		tick_ms++;
+	}
+}
+
+
+int main(void)
+{
+
+	init();
 
 	//ENABLE TIMER NVIC
 	NVIC_SetPriority(TIM1_UP_IRQn,0X00);
 	NVIC_EnableIRQ(TIM1_UP_IRQn);
 
+	uint8_t data[4] = {0x00,0x01,0x02,0x03};
 	//write_single(0xD0,0x00,0x00);
-	//write_single(0xD0,0x01,0x00);
-	//write_single(0xD0,0x02,0x00);
-	////write_single(0xD0,0x03,0x07);
-	//write_single(0xD0,0x00,0x07);
-	//(0xD0,0X00);
-	//delay_ms(1);
-	//ENABLE ACKS
-	//set_pointer(0xD0,0X00);
-	//read(0xD0,4);
+	write_bytes(0xD0,0x00,data,4);
 
 	while(1){
 
-		set_pointer(0xD0,0X00);
-		read(0xD0,4);
+		write_memp(0xD0,0X00);
+		read_bytes(0xD0,buffer,BUFFER_SIZE);
 		seconds = BCD_to_decimal(buffer[0]);
 		minutes = BCD_to_decimal(buffer[1]);
 		hours = BCD_to_decimal(buffer[2]);
-		test+=1;
+
 
 
 	}
